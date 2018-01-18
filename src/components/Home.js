1b968f30a5b490bc4f458225bffdb558035bac7d
Duo Liu <cs.duoliu@gmail.com>
2018-01-17 23:32:55 -0500

    loading geolocation state finished

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Tabs, Button } from 'antd';
 import { GEO_OPTIONS } from "../constants";
+import { Spin } from 'antd';
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
@@ -8,12 +9,13 @@ const operations = <Button>Extra Action</Button>;
 export class Home extends React.Component {
     state = {
         loadingGeoLocation: false,
+        error: '',
     }
     //do resource loading should be in this life circle
     //No need to use arrow function, because it is automatically binded for life circle functions
     //Only user defined needs them.
     componentDidMount(){
-        this.setState({loadingGeoLocation: true});
+        this.setState({loadingGeoLocation: true, error: ''});
         this.getGeolocation();
     }
 
@@ -25,21 +27,28 @@ export class Home extends React.Component {
                 GEO_OPTIONS,
             );
         } else {
-            /* geolocation IS NOT available */
+            this.setState({error: 'Geolocation is Not supported in this browser '});
         }
     }
 
     onSuccessLoadGeolocation = (position) => {
         console.log(position);
-        this.setState({ loadingGeoLocation: false});
+        this.setState({ loadingGeoLocation: false, error: ''});
     }
 
     onFailedLoadGeolocation = () => {
-        this.setState({ loadingGeoLocation: false});
+        this.setState({ loadingGeoLocation: false, error: 'Failed to load geolocation'});
+
     }
 
     getGalleryContent = () => {
-        return this.state.loadingGeoLocation ? <span>loading geolocation</span> : null;
+        if (this.state.error) {
+            return <div>{this.state.error}</div>
+        } else if (this.state.loadingGeoLocation) {
+            return <Spin tip="Loading geolocation..."/>;
+        } else {
+            return
+        }
     }
     //do clear resource should be in willMount life circle
     //if we want to insert a java script we need to use {} and it must return a value
